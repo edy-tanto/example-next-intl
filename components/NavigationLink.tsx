92001712681e7373dd3cd6d64166a42fcd298b16
Edy Tanto <edytanto.chai@gmail.com>
2024-08-26 10:29:13 +0700

    first init

---
+++ components/NavigationLink.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import {useSelectedLayoutSegment} from 'next/navigation';
+import {ComponentProps} from 'react';
+import type {pathnames} from '@/config';
+import {Link} from '@/navigation';
+
+export default function NavigationLink<
+  Pathname extends keyof typeof pathnames
+>({href, ...rest}: ComponentProps<typeof Link<Pathname>>) {
+  const selectedLayoutSegment = useSelectedLayoutSegment();
+  const pathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : '/';
+  const isActive = pathname === href;
+
+  return (
+    <Link
+      aria-current={isActive ? 'page' : undefined}
+      className={'inline-block px-2 py-3 transition-colors'}
+      href={href}
+      {...rest}
+    />
+  );
+}
